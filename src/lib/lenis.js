30a75130a90aff4091b09fcc1221370e1d5d528c
Thomas Filhol <thomas.filhol@edu.devinci.fr>
2025-05-17 18:34:43 +0200

    Setup prod

--- src/lib/lenis.js
+++
@@ -1,9 +0,0 @@
-import Lenis from '@studio-freight/lenis';
-
-export const lenis = new Lenis();
-
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-requestAnimationFrame(raf);
